aadb4d74b4d1b460914d500c97a6b054de9acbff
Milos Roknic <roknic.milos.994@gmail.com>
2023-06-17 16:38:58 +0200

    Update example.env and entrypoint.sh

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -5,10 +5,10 @@ set -e
 . /app/scripts/utils.sh
 
 export REACT_APP_APP_URL=$APP_BASE_URL
-export REACT_APP_API_URL="$API_BASE_URL/api/v1"
+export REACT_APP_API_URL=$API_BASE_URL
 
 npm install
-if $(bool "$APP_DEV_SERVER"); then
+if bool "$APP_DEV_SERVER"; then
   printc "Starting development server...\n" "info"
   npm start
 else
