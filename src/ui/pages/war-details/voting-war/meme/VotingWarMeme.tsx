7672f09696811471add4b0aa9fe098bb431d7307
Milos Roknic <roknic.milos.994@gmail.com>
2023-07-08 15:15:19 +0200

    Create StarRating component for Voting War page

--- src/ui/pages/war-details/voting-war/meme/VotingWarMeme.tsx
+++ src/ui/pages/war-details/voting-war/meme/VotingWarMeme.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { Meme } from "../../../../../models/Meme";
 import styles from "./VotingWarMeme.module.scss";
+import { StarRating } from "../star-rating/StarRating";
+
 
 interface VotingWarMemeProps {
   meme: Meme;
@@ -8,12 +10,13 @@ interface VotingWarMemeProps {
 
 export function VotingWarMeme({ meme }: VotingWarMemeProps) {
   return (
-    <div className={ styles.meme }>
+    <div className={styles.meme}>
       <img
-        className={ styles.memeImage }
-        src={ meme.image }
-        alt={ `meme ${ meme.id }` }
-        onClick={ () => window.location.href = meme.image }/>
+        className={styles.memeImage}
+        src={meme.image}
+        alt={`meme ${meme.id}`}
+        onClick={() => window.location.href = meme.image}/>
+      <StarRating/>
     </div>
   );
 }
