bdbcc1daa8639009ef6fbebed3d1156656acd9d3
Milos Roknic <roknic.milos.994@gmail.com>
2023-07-23 22:44:53 +0200

    Implement necessary CRUD operations for Votes

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,5 +1,6 @@
 export interface User {
   token: string;
+  id: number;
   email: string;
   firstName: string;
   lastName: string;
--- src/ui/pages/war-details/voting-war/star-rating/StarRating.tsx
+++ src/ui/pages/war-details/voting-war/star-rating/StarRating.tsx
@@ -1,38 +1,86 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./StarRating.module.scss";
 import starFilledSVG from "../../../../../assets/starFilled.svg";
-import starHollowSVG from "../../../../../assets/starHollow.svg";
+import starHollowBlackSVG from "../../../../../assets/starHollowBlack.svg";
+import starHollowRedSVG from "../../../../../assets/starHollowRed.svg";
+import { voteService } from "../../../../../services/voteService";
+import { useAuth } from "../../../../../context/authContext";
+import { Vote } from "../../../../../models/Vote";
 
 
-export function StarRating() {
+interface StarRatingProps {
+  memeID: number;
+}
+
+export function StarRating({ memeID }: StarRatingProps) {
   const STAR_COUNT = 5;
-  const [rating, setRating] = useState(0);
-  const [hoverRating, setHoverRating] = useState(0);
+  const { user } = useAuth();
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [vote, setVote] = useState<Vote | null>(null);
+  const [errorMessage, setErrorMessage] = useState<string>("");
+  const [rating, setRating] = useState<number>(0);
+  const [hoverRating, setHoverRating] = useState<number>(0);
 
-  const handleStarHover = (index: number) => {
+  async function loadExistingRating() {
+    try {
+      const vote = await voteService.getVote(user!.id, memeID);
+      if (vote) {
+        setRating(vote.score);
+        setVote(vote);
+      }
+    } catch (e) {
+      setErrorMessage("Error occurred while checking existing rating for this meme");
+    }
+    setIsLoading(false);
+  }
+
+  useEffect(() => {
+    loadExistingRating();
+  }, [user]);
+
+  function handleStarHover(index: number) {
     setHoverRating(index + 1);
-  };
+  }
 
-  const handleStarClick = (index: number) => {
-    setRating(index + 1);
-  };
+  async function handleStarClick(index: number) {
+    const newRating = index + 1;
+    setRating(newRating);
+    try {
+      if (vote) {
+        await voteService.updateVote(vote.id, newRating);
+      } else {
+        await voteService.createVote(memeID, newRating);
+      }
+    } catch (e) {
+      setErrorMessage("Error occurred while submitting a rating for this meme");
+    }
+    setIsLoading(false);
+  }
 
-  const handleMouseLeave = () => {
+  function handleMouseLeave() {
     setHoverRating(0);
-  };
+  }
+
+  function getRegularStarSVG(index: number): string {
+    const shouldReturnFilledSVG = isLoading || index < (hoverRating || rating);
+    return shouldReturnFilledSVG ? starFilledSVG : errorMessage ? starHollowRedSVG : starHollowBlackSVG;
+  }
 
   return (
-    <div className={styles.starRow} onMouseLeave={handleMouseLeave}>
-      {[...Array(STAR_COUNT)].map((_, index) => (
-        <img
-          key={index}
-          onMouseEnter={() => handleStarHover(index)}
-          onClick={() => handleStarClick(index)}
-          className={styles.star}
-          src={index < (hoverRating || rating) ? starFilledSVG : starHollowSVG}
-          alt="star"
-        />
-      ))}
+    <div>
+      <div className={styles.starRow} onMouseLeave={handleMouseLeave}>
+        {[...Array(STAR_COUNT)].map((_, index) => (
+          <img
+            key={index}
+            onMouseEnter={() => handleStarHover(index)}
+            onClick={() => handleStarClick(index)}
+            className={isLoading ? styles.starLoading : styles.star}
+            src={getRegularStarSVG(index)}
+            alt="star"
+          />
+        ))}
+      </div>
+      {errorMessage && <p className={styles.errorMessage}>{errorMessage}</p>}
     </div>
   );
 }
